93a2c6c5fb761f05d7a37b40271e5f15445f2690
anjalis-ingh <96373072+anjalis-ingh@users.noreply.github.com>
2022-01-03 22:20:23 -0600

    7th commit
    - Fixed the volume sliders to work and got the positioning controlled

--- scripts.js
+++ scripts.js
@@ -1,40 +1,89 @@
+$(document).ready(function() {
 
-// The Drop Down Menu
-(function(){
+    // Player Button
+    $("#btn").on("click", function (e) {
+        var audio = $("#audio")[0];
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
 
-  $("#list").on("click", function() {
-    $(".notepad").fadeToggle( "fast");
-  });
+    // Volume Bar
+    $("#volume").on("change input", function () {
+        var audio  =  $("#audio")[0];
+        audio.volume = $(this).val();
+    });
 
-})();
+    $(function() {
 
-// Task List
-var count = 5;
+      var transitionEnd = 'transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd';
 
-add_task(); // Call the add_task function
-delete_task(); // Call the delete_task function
 
-function add_task() {
+      $( '.play-btn' ).click(function( e ) {
 
-  $('.add-new-task').submit(function(){
-    var new_task = $('.add-new-task input[name=new-task]').val();
-    count = count + 1;
+        e.preventDefault();
 
-    var print = '<li><span>' + new_task + '</span><button id="' + count + '" class="delete-button">X</button></li>';
+        if ( $(this).hasClass('stop') ) {
 
-    if(new_task !== ''){
-      $('.add-new-task input[name=new-task]').val('');
-      $(print).appendTo('.task-list ul').hide().fadeIn();
-      delete_task();
-    }
-    return false;
-  });
-}
+          $( this ).removeClass( 'stop' )
+                   .addClass( 'to-play' );
 
-function delete_task() {
-  $('.delete-button').click(function(){
-    var current_element = $(this);
-    var id = $(this).attr('id');
-    current_element.parent().fadeOut("fast", function() { $(this).remove(); });
-  });
-}
+        } else if ( !$(this).hasClass('to-play') ) {
+
+          $( this ).addClass( 'stop' );
+
+        }
+
+      });
+
+    $( document ).on( transitionEnd, '.to-play', function() {
+
+      $( this ).removeClass( 'to-play' );
+
+    });
+
+    });
+
+  // The Drop Down Menu
+  (function(){
+
+    $("#list").on("click", function() {
+      $(".notepad").fadeToggle( "fast");
+    });
+
+  })();
+
+  // Task List
+  var count = 5;
+
+  add_task();
+  delete_task();
+
+  function add_task() {
+
+    $('.add-new-task').submit(function(){
+      var new_task = $('.add-new-task input[name=new-task]').val();
+      count = count + 1;
+
+      var print = '<li><span>' + new_task + '</span><button id="' + count + '" class="delete-button">X</button></li>';
+
+      if(new_task !== ''){
+        $('.add-new-task input[name=new-task]').val('');
+        $(print).appendTo('.task-list ul').hide().fadeIn();
+        delete_task();
+      }
+      return false;
+    });
+  }
+
+  function delete_task() {
+    $('.delete-button').click(function(){
+      var current_element = $(this);
+      var id = $(this).attr('id');
+      current_element.parent().fadeOut("fast", function() { $(this).remove(); });
+    });
+  }
+
+});
