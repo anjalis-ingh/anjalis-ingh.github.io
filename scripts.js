7d94edda8c98a8f9957973e775c73615246e1816
anjalis-ingh <96373072+anjalis-ingh@users.noreply.github.com>
2022-01-14 04:39:41 -0600

    almost final version
    - added and finalized the left menu audios for nature, park, library, and home
    - fix the alignment for the captions and sound icons
    - added the right menus on the four slides mentioned before
    - changed left menu span color
    - vertical snap slider now with no dots

--- scripts.js
+++ scripts.js
@@ -55,6 +55,151 @@ $(document).ready(function() {
         }
     });
 
+    $("#btn6").on("click", function (e) {
+        var audio = $("#audio6")[0];
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+
+
+    $("#btn7").on("click", function (e) {
+        var audio = $("#audio7")[0];
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+
+    $("#btn8").on("click", function (e) {
+        var audio = $("#audio8")[0];
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+
+    $("#btn9").on("click", function (e) {
+        var audio = $("#audio9")[0];
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+
+    $("#btn10").on("click", function (e) {
+        var audio = $("#audio10")[0];
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+
+    $("#btn11").on("click", function (e) {
+        var audio = $("#audio11")[0];
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+
+    $("#btn12").on("click", function (e) {
+        var audio = $("#audio12")[0];
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+
+    $("#btn13").on("click", function (e) {
+        var audio = $("#audio13")[0];
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+
+    $("#btn14").on("click", function (e) {
+        var audio = $("#audio14")[0];
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+
+    $("#btn15").on("click", function (e) {
+        var audio = $("#audio15")[0];
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+
+    $("#btn16").on("click", function (e) {
+        var audio = $("#audio16")[0];
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+
+    $("#btn17").on("click", function (e) {
+        var audio = $("#audio17")[0];
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+
+    $("#btn18").on("click", function (e) {
+        var audio = $("#audio18")[0];
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+
+    $("#btn19").on("click", function (e) {
+        var audio = $("#audio19")[0];
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+
+    $("#btn20").on("click", function (e) {
+        var audio = $("#audio20")[0];
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+
+    $("#btn21").on("click", function (e) {
+        var audio = $("#audio21")[0];
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+
     // End of Player Buttons
 
     // Volume Bars
@@ -88,7 +233,88 @@ $(document).ready(function() {
         audio.volume = $(this).val();
     });
 
-      // End of the Volume Bars
+    $("#volume6").on("change input", function () {
+        var audio  =  $("#audio6")[0];
+        audio.volume = $(this).val();
+    });
+
+    $("#volume7").on("change input", function () {
+        var audio  =  $("#audio7")[0];
+        audio.volume = $(this).val();
+    });
+
+    $("#volume8").on("change input", function () {
+        var audio  =  $("#audio8")[0];
+        audio.volume = $(this).val();
+    });
+
+    $("#volume9").on("change input", function () {
+        var audio  =  $("#audio9")[0];
+        audio.volume = $(this).val();
+    });
+
+    $("#volume10").on("change input", function () {
+        var audio  =  $("#audio10")[0];
+        audio.volume = $(this).val();
+    });
+
+    $("#volume11").on("change input", function () {
+        var audio  =  $("#audio11")[0];
+        audio.volume = $(this).val();
+    });
+
+    $("#volume12").on("change input", function () {
+        var audio  =  $("#audio12")[0];
+        audio.volume = $(this).val();
+    });
+
+    $("#volume13").on("change input", function () {
+        var audio  =  $("#audio13")[0];
+        audio.volume = $(this).val();
+    });
+
+    $("#volume14").on("change input", function () {
+        var audio  =  $("#audio14")[0];
+        audio.volume = $(this).val();
+    });
+
+    $("#volume15").on("change input", function () {
+        var audio  =  $("#audio15")[0];
+        audio.volume = $(this).val();
+    });
+
+    $("#volume16").on("change input", function () {
+        var audio  =  $("#audio16")[0];
+        audio.volume = $(this).val();
+    });
+
+    $("#volume17").on("change input", function () {
+        var audio  =  $("#audio17")[0];
+        audio.volume = $(this).val();
+    });
+
+    $("#volume18").on("change input", function () {
+        var audio  =  $("#audio18")[0];
+        audio.volume = $(this).val();
+    });
+
+    $("#volume19").on("change input", function () {
+        var audio  =  $("#audio19")[0];
+        audio.volume = $(this).val();
+    });
+
+    $("#volume20").on("change input", function () {
+        var audio  =  $("#audio20")[0];
+        audio.volume = $(this).val();
+    });
+
+    $("#volume21").on("change input", function () {
+        var audio  =  $("#audio21")[0];
+        audio.volume = $(this).val();
+    });
+
+
+    // End of the Volume Bars
 
     $(function() {
 
@@ -120,11 +346,24 @@ $(document).ready(function() {
 
     });
 
+
   // The Drop Down Menu
   (function(){
 
     $("#list").on("click", function() {
       $(".notepad").fadeToggle( "fast");
+    });
+
+    $("#list1").on("click", function() {
+      $(".notepad1").fadeToggle( "fast");
+    });
+
+    $("#list2").on("click", function() {
+      $(".notepad2").fadeToggle( "fast");
+    });
+
+    $("#list3").on("click", function() {
+      $(".notepad3").fadeToggle( "fast");
     });
 
   })();
@@ -137,7 +376,7 @@ $(document).ready(function() {
 
   function add_task() {
 
-    $('.add-new-task').submit(function(){
+    $(document).on('submit', '.add-new-task', function(){
       var new_task = $('.add-new-task input[name=new-task]').val();
       count = count + 1;
 
@@ -161,3 +400,100 @@ $(document).ready(function() {
   }
 
 });
+
+
+// Task List 1
+
+var count = 5;
+
+add_task();
+delete_task();
+
+function add_task() {
+
+  $(document).on('submit', '.add-new-task1', function(){
+    var new_task = $('.add-new-task1 input[name=new-task1]').val();
+    count = count + 1;
+
+    var print = '<li><span>' + new_task + '</span><button id="' + count + '" class="delete-button">X</button></li>';
+
+    if(new_task !== ''){
+      $('.add-new-task1 input[name=new-task1]').val('');
+      $(print).appendTo('.task-list1 ul').hide().fadeIn();
+      delete_task();
+    }
+    return false;
+  });
+}
+
+function delete_task() {
+  $('.delete-button').click(function(){
+    var current_element = $(this);
+    var id = $(this).attr('id');
+    current_element.parent().fadeOut("fast", function() { $(this).remove(); });
+  });
+}
+
+// Task List 2
+
+var count = 5;
+
+add_task();
+delete_task();
+
+function add_task() {
+
+  $(document).on('submit', '.add-new-task2', function(){
+    var new_task = $('.add-new-task2 input[name=new-task2]').val();
+    count = count + 1;
+
+    var print = '<li><span>' + new_task + '</span><button id="' + count + '" class="delete-button">X</button></li>';
+
+    if(new_task !== ''){
+      $('.add-new-task2 input[name=new-task2]').val('');
+      $(print).appendTo('.task-list2 ul').hide().fadeIn();
+      delete_task();
+    }
+    return false;
+  });
+}
+
+function delete_task() {
+  $('.delete-button').click(function(){
+    var current_element = $(this);
+    var id = $(this).attr('id');
+    current_element.parent().fadeOut("fast", function() { $(this).remove(); });
+  });
+}
+
+// Task List 3
+
+var count = 5;
+
+add_task();
+delete_task();
+
+function add_task() {
+
+  $(document).on('submit', '.add-new-task3', function(){
+    var new_task = $('.add-new-task3 input[name=new-task3]').val();
+    count = count + 1;
+
+    var print = '<li><span>' + new_task + '</span><button id="' + count + '" class="delete-button">X</button></li>';
+
+    if(new_task !== ''){
+      $('.add-new-task3 input[name=new-task3]').val('');
+      $(print).appendTo('.task-list3 ul').hide().fadeIn();
+      delete_task();
+    }
+    return false;
+  });
+}
+
+function delete_task() {
+  $('.delete-button').click(function(){
+    var current_element = $(this);
+    var id = $(this).attr('id');
+    current_element.parent().fadeOut("fast", function() { $(this).remove(); });
+  });
+}
